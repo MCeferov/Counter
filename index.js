c76a033083fc0f33524751742dd828982384f343
MCeferov <misomanxid@gmail.com>
2024-02-08 20:10:32 +0400

    Counter

---
+++ index.js
@@ -0,0 +1,5 @@
+  appCounter = document.querySelector('.count-title'),
+  appDecrement = document.querySelector('.app-decrement'),
+  appIncrement = document.querySelector('.app-increment'),
+  appReset = document.querySelector('.app-reset');
+
